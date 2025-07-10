1f4f3b196e3e13a426415de3ad852e7b8564b82f
LEO <li@130lab.com>
2025-07-10 13:55:42 +0000

    a

--- scripts/.ipynb_checkpoints/install-terraform-checkpoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-apt-get update && apt-get install -y gnupg software-properties-common wget
-wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor > /usr/share/keyrings/hashicorp-archive-keyring.gpg
-gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
-apt update && apt-get install terraform && rm -rf /var/lib/apt/lists/*
-terraform -help
